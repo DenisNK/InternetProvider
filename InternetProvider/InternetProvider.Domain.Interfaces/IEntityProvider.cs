a6615f74cdd04b542b3c572ec654ae451a00a031
DenisNK <1222den@mail.ru>
2019-10-26 19:40:33 +0300

    Add InternetProvider.Domain.Interfaces

---
+++ InternetProvider/InternetProvider.Domain.Interfaces/IEntityProvider.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace InternetProvider.Domain.Interfaces
+{
+    public interface IEntityProvider<TEntity> where TEntity: IEntity<int>
+    {
+        void Create(TEntity entity);
+        void Update(TEntity entity);
+        void Delete(TEntity entity);
+        TType DeleteById<TType>(TType id) where TType : IEntity<int>;
+        TEntity GetSingle(Expression<Func<TEntity, bool>> filter);
+        IEnumerable<TEntity> GetManyFiltered(Expression<Func<TEntity, bool>> filter);
+    }
+}
---
+++ InternetProvider/InternetProvider.Domain.Interfaces/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿namespace InternetProvider.Domain.Interfaces
+{
+    public interface IUnitOfWork
+    {
+        IEntityProvider<User> Users { get; }
+        IEntityProvider<Role> Roles { get; }
+        IEntityProvider<Tariff> Tariffs { get; }
+        IEntityProvider<Service> Services { get; }
+        IEntityProvider<UserBalance> UserBalances { get; }
+        IEntityProvider<UserTariffBalance> UserTariffBalances { get; }
+        void Commit();
+    }
+}
